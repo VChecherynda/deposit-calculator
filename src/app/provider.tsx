ee556d28d4efaabae0a7ee56cb6e09ca37149ad3
Vadym Checherynda <checherinda.vadym@gmail.com>
2024-10-02 15:55:39 +0300

    Fix error

--- src/app/provider.tsx
+++ src/app/provider.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-// import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { Suspense } from 'react';
 
 const queryClient = new QueryClient();
 
@@ -10,8 +10,7 @@ queryClient.invalidateQueries({ queryKey: ['fromCurrency'] });
 export const Providers = ({ children }: { children: JSX.Element }) => {
     return (
         <QueryClientProvider client={queryClient}>
-            {children}
-            {/* <ReactQueryDevtools initialIsOpen={false} /> */}
+            <Suspense>{children}</Suspense>
         </QueryClientProvider>
     );
 };
